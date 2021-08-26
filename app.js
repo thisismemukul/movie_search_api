a32d600c8dbb9b5dce67272f80225ef6e8bf0862
Mukul Saini <thisismemukul@gmail.com>
2021-08-26 22:49:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var request = require("request");
+var app = express();
+
+//removing ejs extension
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+
+
+
+
+app.get('/results', function(req, res) {
+    request("https://www.omdbapi.com/?apikey=253f736&s=star", function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            res.send(data["Search"]);
+        }
+    });
+});
+
+app.get("*", function(req, res) {
+    res.render("404");
+});
+app.listen(5500, function() {
+    console.log("Server started at port 5500");
+});
