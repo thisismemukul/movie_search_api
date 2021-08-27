dfb58cd79e35145a6fe729253e838b2867c5ff00
Mukul Saini <thisismemukul@gmail.com>
2021-08-28 00:23:49 +0530

    css

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-    console.log(query);
-
     var query = req.query.search;
-
-    console.log(query);
+    if (typeof query !== "undefined") {
+        var query = req.query.search;
+    } else {
+        var query = "Harry";
+    }
     var url = "https://www.omdbapi.com/?apikey=253f736&s=" + query;
     request(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             const data = JSON.parse(body);
             // res.send(data["Search"][0]);
-            // res.send(data);
-            res.render("home", { data: data });
-
+            //  res.send(data["Response"]);
+            // console.log(data["Response"]);
+            res.render("home", { data: data, query: query });
         }
     });
 });
@@ -27,6 +28,8 @@ app.get('/', function(req, res) {
 app.get("*", function(req, res) {
     res.render("404");
 });
-app.listen(5500, function() {
-    console.log("Server started at port 5500");
-});
+// app.listen(5500, function() {
+//     console.log("Server started at port 5500");
+// });
+const port = process.env.PORT || '5500';
+app.listen(port, () => console.log(`server on ${port}`));
